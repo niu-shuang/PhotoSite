b819bcee40d44bcce1ef59b43ad5ef51c25a2131
niushuang <shuang.niu@funplus.biz>
2022-07-24 02:59:56 +0900

    modified footer

--- Proj/resources/views/base/footer.blade.php
+++ Proj/resources/views/base/footer.blade.php
@@ -1,4 +1,5 @@
 <div class="container text-center">
-    <span class="text-light">auction site</span><br>
+    <span class="text-light">fujisan</span><br>
+    <a class="text-light nav-link" href="http://homepage.local/">会社ホームページ</a>
     <span class="text-light">2022</span>
 </div>
